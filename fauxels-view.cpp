8455c8771311b8fc1f376d4e9c6216b601db89be
Kevin Alford <zeroeth@pixelflow.org>
2015-07-23 14:51:00 -0700

    Larger values for longer led strands

--- fauxels-view.cpp
+++ fauxels-view.cpp
@@ -14,13 +14,13 @@ SDL_Renderer* renderer = NULL;
 SDL_Event event;
 
 // List of leds per view and of view sizes
-uint8_t fauxel_leds[256]       = {0};
-uint8_t fauxel_view_size [256] = {0};
-uint8_t fauxel_view_start[256] = {0};
+uint16_t fauxel_leds[256]       = {0};
+uint16_t fauxel_view_size [256] = {0};
+uint16_t fauxel_view_start[256] = {0};
 uint8_t fauxel_view_count = 0;
 
 extern "C" {
-	void register_view(uint8_t start, uint8_t length)
+	void register_view(uint16_t start, uint16_t length)
 	{
 		// Track instances and assign leds to each view once.
 		// TODO use static lists on the view classes.
@@ -47,10 +47,10 @@ extern "C" {
 
 		uint16_t center_x = (SCREEN_WIDTH / fauxel_view_count) * view - (SCREEN_WIDTH / fauxel_view_count / 2) ;
 		uint16_t center_y = SCREEN_HEIGHT / 2;
-		uint16_t x = sin((360.0/view_led_count * view_led_position) * (M_PI/180)) * view_led_count * 5 + center_x;
-		uint16_t y = cos((360.0/view_led_count * view_led_position) * (M_PI/180)) * view_led_count * 5 + center_y;
+		uint16_t x = sin((360.0/view_led_count * view_led_position) * (M_PI/180)) * (SCREEN_WIDTH / fauxel_view_count / 2 - 25) + center_x;
+		uint16_t y = cos((360.0/view_led_count * view_led_position) * (M_PI/180)) * (SCREEN_WIDTH / fauxel_view_count / 2 - 25) + center_y;
 		// Could derive from # of views and led count to squeeze into space
-		uint16_t radius = 10;
+		uint16_t radius = 20;
 
 		filledCircleRGBA( renderer, x, y, radius, r, g, b, 0xFF );
 
